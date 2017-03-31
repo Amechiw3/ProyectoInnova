efcce8c73075836e02c948232112026554fd592a
Amechiw3 <martinfierro97@hotmail.com>
2017-03-31 14:57:35 -0700

    Clases para login y acceso, template web

---
+++ ProyectoInnova/ProyectoInnovaDESK/Controllers/AccesoManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProyectoInnovaDESK.Models;
+
+namespace ProyectoInnovaDESK.Controllers
+{
+    public class AccesoManager
+    {
+        /// <summary>
+        /// Funcion encargada de registrar el acceso de un usuario al sistema
+        /// </summary>
+        /// <param name="acceso">Recibe los datos de acceso</param>
+        public static void RegistrarAcceso(Acceso acceso)
+        {
+            try
+            {
+                var ctx = new DataModel();
+                ctx.Accesos.Add(acceso);
+                ctx.Usuarios.Attach(acceso.usuario);
+                ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ProyectoInnova/ProyectoInnovaDESK/Controllers/UsuarioManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProyectoInnovaDESK.Tools;
+using ProyectoInnovaDESK.Models;
+using ProyectoInnovaDESK.Controllers.Helpers;
+
+namespace ProyectoInnovaDESK.Controllers
+{
+    public class UsuarioManager
+    {
+        public static UsuarioHelper Autentificar(String sUsuario, String sPassword)
+        {
+            UsuarioHelper uHelper = new UsuarioHelper();
+            Usuario user = BuscarPorEmail(sUsuario);
+            if (user != null)
+            {
+                if (user.sPassword == LoginTool.GetMD5(sPassword))
+                {
+                    uHelper.usuario = user;
+                    uHelper.esValido = true;
+                    Acceso nAcceso = new Acceso();
+                    nAcceso.usuario = user;
+                    AccesoManager.RegistrarAcceso(nAcceso);
+                }
+                else
+                {
+                    uHelper.sMensaje = "Datos Incorrectos";
+                }
+            }
+            return uHelper;
+        }
+
+        private static Usuario BuscarPorEmail(string Email)
+        {
+            try
+            {
+                using (var ctx = new DataModel())
+                {
+                    return ctx.Usuarios.Include("rol")
+                        .Include("rol.PermisosNegados")
+                        .Include("rol.PermisosNegados.permiso")
+                        .Where(r => r.sUsuario == Email).FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void RegistrarNuevoUsuario(Usuario nUsuario)
+        {
+            try
+            {
+                using (var ctx = new DataModel())
+                {
+                    ctx.Usuarios.Add(nUsuario);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
