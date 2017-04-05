7138196a352994291961fcc091b7ab796ba1fec8
EvelinMartinez <evelin_fda@hotmail.com>
2017-04-05 09:27:30 -0700

    frmmunicipios

---
+++ ProyectoInnova/ProyectoInnovaDESK/Views/frnAddMunicipios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ProyectoInnovaDESK.Models;
+using ProyectoInnovaDESK.Controllers;
+using System.Text.RegularExpressions;
+
+namespace ProyectoInnovaDESK.Views
+{
+    public partial class frnAddMunicipios : Form
+    {
+        public frnAddMunicipios()
+        {
+            InitializeComponent();
+        }
+
+        private void frnAddMunicipios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
